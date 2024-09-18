1b64ac76e11371d0224fe3b9794405a9a67bb578
Edwin Vargas <vargas11742@gmail.com>
2024-09-18 08:05:44 -0500

    feat: widget asset card

--- lib/widgets/asset_card.dart
+++ lib/widgets/asset_card.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
-
 import '/widgets/image_view.dart';
 import '/widgets/video_view.dart';
 
-class AssetThumbnail extends StatelessWidget {
-  const AssetThumbnail({
+class AssetCard extends StatelessWidget {
+  const AssetCard({
     super.key,
     required this.asset,
   });
 
   final AssetEntity asset;
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Uint8List>(
       future: asset.thumbnailData.then((value) => value!),
       builder: (_, snapshot) {
         final bytes = snapshot.data;
-        if (bytes == null) return const CircularProgressIndicator();
+        if (bytes == null) return const Image(image: AssetImage('assets/image_not_found.png'));
         return InkWell(
           onTap: () {
             Navigator.push(
@@ -49,15 +47,25 @@ class AssetThumbnail extends StatelessWidget {
               ),
               // Display a Play icon if the asset is a video
               if (asset.type == AssetType.video)
-                Center(
+                Positioned(
+                  bottom: 5,
+                  right: 5,
                   child: Container(
-                    color: Colors.blue,
-                    child: const Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
-                    ),
+                    decoration: const BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: Colors.blueAccent,
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                      ),
+                    ]),
+                    child: Text(Duration(seconds: asset.duration)
+                        .toString()
+                        .split('.')
+                        .first
+                        .padLeft(8, "0")
+                        .substring(1)),
                   ),
-                ),
+                )
             ],
           ),
         );
